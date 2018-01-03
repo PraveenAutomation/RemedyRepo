e424e2967123a1f6f1841c6148ccd0678a236476
PraveenAutomation <praveen.seleniumautomation@gmail.com>
2018-01-03 15:57:44 +0530

    First Commit

---
+++ com.test/src/main/java/testNg/TestNgBasics.java
@@ -0,0 +1,42 @@
+package testNg;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNgBasics {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\pk.mishra\\Desktop\\Shift Folder 5 April'16\\KP Fouram\\Jars\\geckodriver.exe");
+		
+		driver =new FirefoxDriver();
+		
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://www.remedypartners.com/");
+	}
+	
+	@Test
+	public void remedyTest()
+	{
+		String title=driver.getTitle();
+		System.out.println(title);
+	}
+	
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+
+}
