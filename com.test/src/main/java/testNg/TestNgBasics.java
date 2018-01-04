ad8f3249af4f7aaedd51db5eb1edbfbe094e8296
PraveenAutomation <praveen.seleniumautomation@gmail.com>
2018-01-04 17:10:47 +0530

    Start EC1 Automate

--- com.test/src/main/java/testNg/TestNgBasics.java
+++ com.test/src/main/java/testNg/TestNgBasics.java
@@ -3,14 +3,17 @@ package testNg;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNgBasics {
 	WebDriver driver;
+	Actions action;
 	
 	@BeforeMethod
 	public void setUp()
@@ -37,6 +40,17 @@ public class TestNgBasics {
 	{
 		boolean b=driver.findElement(By.xpath("//a[text()='Get Started']")).isDisplayed();
 		System.out.println(b);
+	}
+	
+	@Test
+	public void logoSize()
+	{
+		Dimension dim=driver.findElement(By.xpath("//img[@class='logo-img']")).getSize();
+		int ht=dim.height;
+		int wd=dim.width;
+		System.out.println("Dimension of Logo is "+dim);
+		System.out.println("Height of logo"+ht);
+		System.out.println("Width of logo"+wd);
 	}
 	
 	
